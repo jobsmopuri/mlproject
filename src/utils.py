1f407ef48917db75ddfd2c4a818e0721ce5433b9
Sivakumar Mopuri <ds.mopuri@gmail.com>
2024-11-26 14:38:25 +0530

    Total project

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -32,4 +32,9 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
     except Exception as e:
         raise CustomException(e,sys)
         
-    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
